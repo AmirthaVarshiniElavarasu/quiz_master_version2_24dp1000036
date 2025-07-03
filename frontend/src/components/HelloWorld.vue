9c2876bfcbb43b5752c8ed14e0bbc7b418bec17f
AmirthaVarshiniElavarasu <amirthavarhini@gmail.com>
2025-07-03 17:15:39 +0530

    testing of frontend and backend connection

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  mounted() {
+    fetch('http://localhost:5000/api/hello')
+      .then(res => res.json())
+      .then(data => {
+        this.message = data.message;
+      })
+  }
+}
+</script>
