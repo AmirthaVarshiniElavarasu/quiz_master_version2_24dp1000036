6187b488bbde1f1bd6d96b04b5ccd18c62eb4608
AmirthaVarshiniElavarasu <amirthavarhini@gmail.com>
2025-08-19 01:53:18 +0530

    clear the error in userdb.py

--- backend/application/userdb.py
+++ backend/application/userdb.py
@@ -1,5 +1,5 @@
 from flask_security import SQLAlchemyUserDatastore
-from application.database import db
-from application.models import User, Role
+from .database import db
+from .models import User, Role
 
 datastore=SQLAlchemyUserDatastore(db,User,Role)
